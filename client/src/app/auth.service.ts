8c02ab89460e8b57ccee4b1bfff48e60e43961bc
halo <cembreyfarquhar@gmail.com>
2019-09-29 22:35:51 -0500

    login/out and getLoggedIn functions created - hardcoded values currently, no http requests

--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { User } from './user';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor() {}
 
-  constructor() { }
+  public login(userInfo: User): void {
+    localStorage.setItem('TOKEN', 'token');
+  }
+
+  public getIsLoggedIn(): boolean {
+    return localStorage.getItem('TOKEN') !== null;
+  }
+
+  public logout(): void {
+    localStorage.removeItem('TOKEN');
+  }
 }
