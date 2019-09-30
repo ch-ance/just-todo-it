d6585cce84c7711204e0122f3db572aabe0076f6
halo <cembreyfarquhar@gmail.com>
2019-09-29 19:04:25 -0500

    nodemon added to dev dependencies; server is running with sanity check

--- api/index.js
+++ api/index.js
@@ -1,1 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(morgan('combined'));
+// SANITY CHECK
+server.get('/', (_req, res) => {
+    res.send('Server is up and running');
+});
+const port = process.env.PORT || 5000;
+server.listen(port, function() {
+    console.log(`\n*** Listening on port ${port} ***\n`);
+});
